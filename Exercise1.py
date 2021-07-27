4fb76b2779f397b44cdbf57d9db4caae3ff9f515
postolakyan <postolakyannver1@gmail.com>
2021-07-27 22:11:11 +0400

    Add docstrings

--- Exercise1.py
+++ Exercise1.py
@@ -4,10 +4,12 @@
 
 
 def stringConvert(word):
-    x=int(len(word)/2)
-    res=(word[x-1:x+2])
-    print(res)
-    
+	'''This function prints the three 
+	letters in the middle of the entered word'''
+	x=int(len(word)/2)
+	res=(word[x-1:x+2])
+	print("The result = "+res)
+	print(stringConvert.__doc__) 
 
 word=input("Please enter an odd word longer than 7  ")
 if len(word)>7 and len(word)%2!=0 and type(word)==type("string"):
@@ -21,10 +23,13 @@ else:
 
 
 def String(word1,word2):
-    mid=word1[:len(word1)//2]
-    mid2=word1[len(word1)//2:]
-    result=mid+word2+mid2
-    print(result)
+	'''This function places the second word in the 
+	middle of the first word and prints it on the screen'''
+	mid=word1[:len(word1)//2]
+	mid2=word1[len(word1)//2:]
+	result=mid+word2+mid2
+	print("The result = ",result)
+	print(String.__doc__)
     
 
 string1= input("Please insert the first string   ")
